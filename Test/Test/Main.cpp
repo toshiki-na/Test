f3f34b2ce27819ddc410b8707802a11f20577d44
toshiki-na <toshiki.nkmr1124@gmail.com>
2025-07-24 18:53:01 +0900

    GoodByeを削除

--- Test/Test/Main.cpp
+++ Test/Test/Main.cpp
@@ -5,10 +5,7 @@ int main()
     //Hello Worldと出力
     std::cout << "Hello World!\n";
 
-    //GoodByeと出力
-    std::cout << "GoodBye\n";
-
-    //HelloWorldと出力
+    //HelloWorldと出力2
     std::cout << "HelloWorld\n";
 
     return 0;
